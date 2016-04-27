277439e8609ac4ec35cbbd733712657591cb7895
Tom Bradley <bradley.tom.r@gmail.com>
2016-04-27 12:43:54 +0100

    Refactors plateau tests

--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -1,19 +1,15 @@
 require 'plateau'
 
 describe Plateau do
-  let(:plateau) { described_class }
+  width = 5
+  height = 10
+  let(:plateau) { described_class.new(width, height) }
 
   it 'should create a grid with the correct width' do
-    width = rand(1..10)
-    height = 1
-    new_plateau = plateau.new(width, height)
-    expect(new_plateau.plateau_map.length).to equal(width)
+    expect(plateau.plateau_map.length).to equal(width)
   end
 
   it 'should create a grid with the correct height' do
-    width = 1
-    height = rand(1..10)
-    new_plateau = plateau.new(width, height)
-    expect(new_plateau.plateau_map[0].length).to equal(height)
+    expect(plateau.plateau_map[0].length).to equal(height)
   end
 end
